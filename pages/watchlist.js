4569bf61206a154ef851ae67aef31eddd611f583
rjlucasm <rjlucasmonteiro@gmail.com>
2021-10-08 17:29:01 -0300

    removed nft

--- pages/watchlist.js
+++ pages/watchlist.js
@@ -72,9 +72,6 @@ export default function Watchlist() {
                 <a href="https://getimpactnow.org/" target="_blank" rel="noopener noreferrer">Get Impact Now</a> - Digital platform that enables responsible citizens to be a part of the decision-making process
             </div>
             <div className="pagescard">
-                <a href="https://zfjzz-4aaaa-aaaah-aasbq-cai.raw.ic0.app/" target="_blank" rel="noopener noreferrer">IC Gallery</a> - A creative metaverse to gather all NFT enthusiasts and enjoy arts on the Internet Computer
-            </div>
-            <div className="pagescard">
                 <a href="https://kdc2c-uaaaa-aaaae-qaafq-cai.ic0.app/" target="_blank" rel="noopener noreferrer">IC Go</a> - Go links for the Internet Computer
             </div>
             <div className="pagescard">
@@ -96,22 +93,10 @@ export default function Watchlist() {
                 <a href="https://icpswap.com/" target="_blank" rel="noopener noreferrer">ICPSwap</a> - The Hub of Future Decentralized Finance
             </div>
             <div className="pagescard">
-                <a href="https://icpuppies.io/" target="_blank" rel="noopener noreferrer">ICPuppies</a> - The first animal NFT collection deployed on the Internet Computer
-            </div>
-            <div className="pagescard">
                 <a href="http://icwallet.org/" target="_blank" rel="noopener noreferrer">ICWallet</a> - Your gateway to Dfinity ecosystem
             </div>
             <div className="pagescard">
-                <a href="https://www.icproject.org/" target="_blank" rel="noopener noreferrer">Infinite Charity Project</a> - A nonprofit charitable org conducting $ICP NFT fundraisers to benefit 501(c)3 charities
-            </div>
-            <div className="pagescard">
-                <a href="https://infinityfrogs.com/" target="_blank" rel="noopener noreferrer">Infinity Frogs</a> - First NFT to bridge ETH and ICP
-            </div>
-            <div className="pagescard">
                 <a href="https://infinityswap.one/" target="_blank" rel="noopener noreferrer">Infinity Swap</a> - A platform to create, stake and swap Dfinity tokens
-            </div>
-            <div className="pagescard">
-                <a href="https://interastrosc.com/" target="_blank" rel="noopener noreferrer">Internet Astronauts</a> - A collection of 10,000 unique digital NFT collectibles only found on the Internet Computer
             </div>
             <div className="pagescard">
                 <a href="https://learnd.dedn.eu/" target="_blank" rel="noopener noreferrer">LEARND</a> - Community driven educational platform running on Internet Computer
@@ -126,13 +111,7 @@ export default function Watchlist() {
                 <a href="https://muga.media/nft" target="_blank" rel="noopener noreferrer">MugaTunes</a> - Music curation and NFT platform
             </div>
             <div className="pagescard">
-                <a href="https://kqre2-2qaaa-aaaad-qamxa-cai.raw.ic0.app/" target="_blank" rel="noopener noreferrer">NFT Studio</a> - Mint, buy and sell 3DWASM NFTs
-            </div>
-            <div className="pagescard">
                 <a href="https://fxnaj-yaaaa-aaaaf-qad3q-cai.raw.ic0.app/" target="_blank" rel="noopener noreferrer">Nuance</a> - Decentralized blogging platform
-            </div>
-            <div className="pagescard">
-                <a href="https://www.origyn.ch/" target="_blank" rel="noopener noreferrer">Origyn</a> - Blending luxury goods, with NFTs by providing digital verifications for physical objects
             </div>
             <div className="pagescard">
                 <a href="https://overchute.com/" target="_blank" rel="noopener noreferrer">Overchute</a> - Decentralized smart-contract application for crowdfunding the release of intellectual property 
@@ -140,9 +119,6 @@ export default function Watchlist() {
             {/* <div className="pagescard">
                 <a href="https://ja7sy-daaaa-aaaai-qaguq-cai.raw.ic0.app/" target="_blank" rel="noopener noreferrer">Portal</a> - Decentralized live streaming service
             </div> */}
-            <div className="pagescard">
-                <a href="https://theswordnft.com/" target="_blank" rel="noopener noreferrer">The Sword</a> - 10K Unique Ninjas NFTs
-            </div>
             <div className="pagescard">
                 <a href="https://voicemarkr.com/shell/map" target="_blank" rel="noopener noreferrer">VoiceMarkr</a> - P2P voice on the Internet Computer
             </div>
