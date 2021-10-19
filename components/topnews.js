7e0c1737e5142b8a444d995df8ac6a6ee0e05bab
rjlucasm <rjlucasmonteiro@gmail.com>
2021-10-19 10:47:04 -0300

    News

--- components/topnews.js
+++ components/topnews.js
@@ -1,11 +1,7 @@
 const Topnews = () => (
     <div className="topnews">
-         <p><a href="https://medium.com/infinityswap/how-the-token-economy-will-power-internet-computer-adoption-information-on-infinityswaps-4e38706bd2ba" target="_blank" rel="noopener noreferrer">How the Token Economy will Power Internet Computer Adoption</a></p>
-         <p><a href="https://toniqlabs.medium.com/introducing-epic-nfts-bc3ecb90a782" target="_blank" rel="noopener noreferrer">Introducing Epic NFTs</a></p>
-         <p><a href="https://dailycoin.com/great-news-for-3d-artists-interactive-nft-platform-has-launched/" target="_blank" rel="noopener noreferrer">Great News for 3D Artists: Interactive NFT Platform Has Launched</a></p>
-         <p><a href="https://medium.com/dfinity/dfinity-developer-grant-program-awards-102-grants-worth-2-04m-b49005d181cf" target="_blank" rel="noopener noreferrer">DFINITY Developer Grant Program Awards 102 Grants Worth $2.04M</a></p>
-         <p><a href="https://www.marketwatch.com/press-release/icme-awarded-grant-from-the-dfinity-foundation-to-build-on-the-internet-computer-2021-10-14" target="_blank" rel="noopener noreferrer">ICME Awarded Grant From the DFINITY Foundation</a></p>
+         <p><a href="https://www.crowdfundinsider.com/2021/10/181778-internet-computer-project-dfinity-dev-grant-program-provides-102-grants-valued-at-2m/" target="_blank" rel="noopener noreferrer">DFINITY Dev Grant Program Provides 102 Grants Valued at $2M+</a></p>
     </div>
 );
-  
+
 export default Topnews;
