c805afce85a8be1f5372d9d5c64d7031760181bb
rjlucasm <rjlucasmonteiro@gmail.com>
2021-10-20 09:34:04 -0300

    Video

--- components/topnews.js
+++ components/topnews.js
@@ -1,6 +1,6 @@
 const Topnews = () => (
     <div className="topnews">
-         <p><a href="https://www.crowdfundinsider.com/2021/10/181778-internet-computer-project-dfinity-dev-grant-program-provides-102-grants-valued-at-2m/" target="_blank" rel="noopener noreferrer">DFINITY Dev Grant Program Provides 102 Grants Valued at $2M+</a></p>
+         <p><a href="https://youtu.be/7WvgON_5XCU" target="_blank" rel="noopener noreferrer">Blockchain Singularity with Dominic Williams</a></p>
     </div>
 );
 
