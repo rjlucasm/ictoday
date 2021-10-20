fc8404052f602d3d02f66561fb363945e7de0314
rjlucasm <rjlucasmonteiro@gmail.com>
2021-10-20 16:34:00 -0300

    News 2

--- components/topnews.js
+++ components/topnews.js
@@ -1,6 +1,7 @@
 const Topnews = () => (
     <div className="topnews">
-         <p><a href="https://youtu.be/7WvgON_5XCU" target="_blank" rel="noopener noreferrer">Blockchain Singularity with Dominic Williams</a></p>
+        <p><a href="https://medium.com/dfinity/secure-scalability-the-internet-computers-peer-to-peer-layer-6662d451f2cc" target="_blank" rel="noopener noreferrer">Secure Scalability: The Internet Computer’s Peer-to-Peer Layer</a></p>
+        <p><a href="https://youtu.be/7WvgON_5XCU" target="_blank" rel="noopener noreferrer">Blockchain Singularity with Dominic Williams</a></p>
     </div>
 );
 
