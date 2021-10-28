a277ea2eb9e3bde6fe5db2cd022b46743544e8e1
rjlucasm <rjlucasmonteiro@gmail.com>
2021-10-28 10:53:47 -0300

    News 28/10

--- components/topnews.js
+++ components/topnews.js
@@ -2,6 +2,7 @@ import Link from 'next/link'
 
 const Topnews = () => (
     <div className="topnews">
+        <p><a href="https://medium.com/dfinity/how-dapp-developers-placing-their-faith-in-total-decentralization-will-inherit-the-world-79419a3e36c9" target="_blank" rel="noopener noreferrer">Why Totally Decentralizing Dapps Wins, And How To Do It</a></p>
         <p><a href="https://medium.com/psychedelic-dao/psychedelic-a-product-studio-dao-on-the-internet-computer-47145fa96700" target="_blank" rel="noopener noreferrer">Psychedelic: A Product Studio DAO on the Internet Computer</a></p>
         <Link href="/news/2021"><a style={{ color: 'inherit' }, {fontWeight: 'bold'}}>+News</a></Link>
     </div>
--- pages/news/2021.js
+++ pages/news/2021.js
@@ -17,6 +17,9 @@ export default function News21() {
             <Link href="/"><a style={{ color: 'inherit' }}>Home</a></Link>
 
             <div className="pagesgrid">
+            <div className="pagescard">10/28 -&nbsp;
+                <a href="https://medium.com/dfinity/how-dapp-developers-placing-their-faith-in-total-decentralization-will-inherit-the-world-79419a3e36c9" target="_blank" rel="noopener noreferrer">Why Totally Decentralizing Dapps Wins, And How To Do It</a>
+            </div>
             <div className="pagescard">10/27 -&nbsp;
                 <a href="https://medium.com/psychedelic-dao/psychedelic-a-product-studio-dao-on-the-internet-computer-47145fa96700" target="_blank" rel="noopener noreferrer">Psychedelic: A Product Studio DAO on the Internet Computer</a>
             </div>
