890372c363f29b812c6a32484e029723bbcb89b0
rjlucasm <rjlucasmonteiro@gmail.com>
2021-11-03 20:47:40 -0300

    Shangai

--- components/topnews.js
+++ components/topnews.js
@@ -2,6 +2,7 @@ import Link from 'next/link'
 
 const Topnews = () => (
     <div className="topnews">
+        <p><a href="https://medium.com/dfinity/the-internet-computer-at-shanghai-international-blockchain-week-4de75b43cf5f" target="_blank" rel="noopener noreferrer">The Internet Computer at Shanghai International Blockchain Week</a></p>
         <p><a href="https://medium.com/psychedelic-dao/announcing-dfinance-joins-psychedelic-and-rebrands-to-sonic-9399ddcb648d" target="_blank" rel="noopener noreferrer">Announcing DFinance Joins Psychedelic and Rebrands to Sonic 👽</a></p>
         <Link href="/news/2021"><a style={{ color: 'inherit' }, {fontWeight: 'bold'}}>+News</a></Link>
     </div>
--- pages/news/2021.js
+++ pages/news/2021.js
@@ -18,6 +18,9 @@ export default function News21() {
 
             <div className="pagesgrid">
             <div className="pagescard">11/03 -&nbsp;
+                <a href="https://medium.com/dfinity/the-internet-computer-at-shanghai-international-blockchain-week-4de75b43cf5f" target="_blank" rel="noopener noreferrer">The Internet Computer at Shanghai International Blockchain Week</a>
+            </div>
+            <div className="pagescard">11/03 -&nbsp;
                 <a href="https://medium.com/psychedelic-dao/announcing-dfinance-joins-psychedelic-and-rebrands-to-sonic-9399ddcb648d" target="_blank" rel="noopener noreferrer">Announcing DFinance Joins Psychedelic and Rebrands to Sonic 👽</a>
             </div>
             <div className="pagescard">10/29 -&nbsp;
