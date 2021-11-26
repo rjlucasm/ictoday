7adb9c8a63cad6934a14479525fc6cedd9f7d8fc
rjlucasm <rjlucasmonteiro@gmail.com>
2021-11-25 22:15:38 -0300

    More News 11/25

--- components/topnews.js
+++ components/topnews.js
@@ -2,8 +2,8 @@ import Link from 'next/link'
 
 const Topnews = () => (
     <div className="topnews">
+        <p><a href="https://cryptoslate.com/3d-nfts-make-their-way-to-the-internet-computer-icp-network/" target="_blank" rel="noopener noreferrer">3D NFTs make their way to the Internet Computer (ICP) network</a></p>
         <p><a href="https://medium.com/dfinity/defi-boom-coming-internet-computer-smart-contracts-can-now-transfer-icp-tokens-c9916ede1060" target="_blank" rel="noopener noreferrer">DeFi Boom Coming: Internet Computer Smart Contracts Can Now Transfer ICP Tokens</a></p>
-        <p><a href="https://cryptomode.com/how-toniq-labs-is-shaping-gamefi-on-the-internet-computer/" target="_blank" rel="noopener noreferrer">How Toniq Labs Is Shaping GameFi on the Internet Computer</a></p>
         <Link href="/news/2021"><a style={{ color: 'inherit' }, {fontWeight: 'bold'}}>+News</a></Link>
     </div>
 );
