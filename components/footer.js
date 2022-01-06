6117f15bf526b0cb5975aa18e2b3355b9009d70f
rjlucasm <rjlucasmonteiro@gmail.com>
2022-01-05 21:08:35 -0300

    Donate

--- components/footer.js
+++ components/footer.js
@@ -3,16 +3,22 @@ const Footer = () => (
         Powered by
         <span>
             <a href="https://dfinity.org/" target="_blank" rel="noopener noreferrer">Dfinity</a>
-        </span> 
+        </span>
 
         <span>
             <a href="https://fleek.co/" target="_blank" rel="noopener noreferrer">Fleek</a>
-        </span> 
+        </span>
 
         <span>
             <a href="https://www.coingecko.com/en/api" target="_blank" rel="noopener noreferrer">Coingecko</a>
-        </span> 
+        </span>
+
+        |
+
+        <span>
+            <a style={{ color: 'blue' }} href="https://ic.rocks/account/5a8d75e42bf41fd4eb91a25ea2bd8fcb9ffbad8bcd2dd7c16f9f9304b702c5fb" target="_blank" rel="noopener noreferrer">Donate</a>
+        </span>
     </footer>
 );
-  
+
 export default Footer;
