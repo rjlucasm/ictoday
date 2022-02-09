b761d7073d2678851bf9fc6fe5154238cbe431fe
rjlucasm <rjlucasmonteiro@gmail.com>
2022-02-09 10:42:51 -0300

    News 02/09

--- pages/news/2022.js
+++ pages/news/2022.js
@@ -17,6 +17,9 @@ export default function News22() {
             <Link href="/"><a style={{ color: 'inherit' }}>Home</a></Link>
 
             <div className="pagesgrid">
+            <div className="pagescard">02/09 -&nbsp;
+                <a href="https://stockstelegraph.com/icp-coin-still-struggling-to-become-bullish/" target="_blank" rel="noopener noreferrer">Internet Computer (ICP)  – Still Struggling to become Bullish</a>
+            </div>
             <div className="pagescard">02/07 -&nbsp;
                 <a href="https://thenewstack.io/internet-computer-where-were-going-we-dont-need-clouds/" target="_blank" rel="noopener noreferrer">Internet Computer: Where We&apos;re Going, We Don&apos;t Need Clouds</a>
             </div>
