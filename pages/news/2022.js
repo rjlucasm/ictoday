319310854c7883118894584c53700e5690fb364f
rjlucasm <rjlucasmonteiro@gmail.com>
2022-03-28 22:39:07 -0300

    News 03/28

--- pages/news/2022.js
+++ pages/news/2022.js
@@ -19,6 +19,9 @@ export default function News22() {
             <Link href="/archive"><a style={{ color: 'inherit' }}>Archives</a></Link>
 
             <div className="pagesgrid">
+            <div className="pagescard">03/28 -&nbsp;
+                <a href="https://beincrypto.com/internet-computer-icp-breaks-out-from-176-day-resistance-line/" target="_blank" rel="noopener noreferrer">Internet Computer (ICP) Breaks Out From 176 Day Resistance Line</a>
+            </div>
             <div className="pagescard">03/24 -&nbsp;
                 <a href="https://invezz.com/news/2022/03/24/is-icp-a-good-buy-opportunity-in-april-2022/" target="_blank" rel="noopener noreferrer">Is ICP a good buy opportunity in April 2022?</a>
             </div>
