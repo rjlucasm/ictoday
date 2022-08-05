e6573969004a60b0439e4f7b990bb1bc52da8792
rjlucasm <rjlucasmonteiro@gmail.com>
2022-08-04 22:36:42 -0300

    News 08/04

--- pages/news/2022.js
+++ pages/news/2022.js
@@ -19,6 +19,9 @@ export default function News22() {
             <Link href="/archive"><a style={{ color: 'inherit' }}>Archives</a></Link>
 
             <div className="pagesgrid">
+            <div className="pagescard">08/04 -&nbsp;
+                <a href="https://medium.com/dfinity/btc-icp-integration-beta-release-the-bitcoin-testnet-api-is-now-available-bab84c378c1e" target="_blank" rel="noopener noreferrer">BTC x ICP Integration Beta Release: The Bitcoin Testnet API Is Now Available</a>
+            </div>
             <div className="pagescard">07/27 -&nbsp;
                 <a href="https://medium.com/dfinity/developer-tooling-and-why-it-matters-for-the-internet-computer-c49779ce8fea" target="_blank" rel="noopener noreferrer">Developer Tooling, and Why It Matters for the Internet Computer</a>
             </div>
