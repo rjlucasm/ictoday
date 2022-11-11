2f920ad8a1e50f2bdc2c5bf856efcfed50f28625
rjlucasm <rjlucasmonteiro@gmail.com>
2022-11-11 18:53:33 -0300

    Footer credit

--- components/footer.js
+++ components/footer.js
@@ -1,3 +1,6 @@
+import Image from 'next/image'
+
+const trippysrc = "https://internetcomputer.today/trippy_ic_logo.svg";
 const Footer = () => (
     <footer className="footer">
         Powered by
@@ -15,9 +18,14 @@ const Footer = () => (
 
         |
 
-        <span>
+        {/* <span>
             <a style={{ color: 'blue' }} href="https://icscan.io/account/5a8d75e42bf41fd4eb91a25ea2bd8fcb9ffbad8bcd2dd7c16f9f9304b702c5fb" target="_blank" rel="noopener noreferrer">Donate</a>
-        </span>
+        </span> */}
+
+        <div className="credits">
+            <a href="https://usertest.icp.page/" target="_blank" rel="noopener noreferrer"><Image src={trippysrc} alt="usertest" width={35} height={15}/></a>
+        </div>
+        
     </footer>
 );
 
