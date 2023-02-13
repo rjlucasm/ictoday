faecc387829b395e0fea917571e0b40edb9a14a4
rjlucasm <rjlucasmonteiro@gmail.com>
2023-02-13 12:43:46 -0300

    NFT update

--- components/sites.js
+++ components/sites.js
@@ -112,8 +112,8 @@ const Sites = () => (
          <p><a href="https://7pnex-saaaa-aaaai-qbhwa-cai.ic0.app/" target="_blank" rel="noopener noreferrer">CetoSwap</a></p>
          <p><a href="https://entrepot.app/" target="_blank" rel="noopener noreferrer">Entrepot</a></p>
          <p><a href="https://nntkg-vqaaa-aaaad-qamfa-cai.ic.fleek.co/" target="_blank" rel="noopener noreferrer">NFT Village</a></p>
-         <p><a href="https://icpunks.com/" target="_blank" rel="noopener noreferrer">ICPunks</a></p>
-         <p><a href="https://dvr6e-lqaaa-aaaai-qam5a-cai.raw.ic0.app/" target="_blank" rel="noopener noreferrer">IC Drip</a></p>
+         <p><a href="https://t5t44-naaaa-aaaah-qcutq-cai.raw.ic0.app/" target="_blank" rel="noopener noreferrer">NFTgeek</a></p>
+         <p><a href="https://tppkg-ziaaa-aaaal-qatrq-cai.raw.ic0.app/" target="_blank" rel="noopener noreferrer">Yumi</a></p>
       </div>
 
       <div className="card">
