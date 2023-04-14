405e70a900e66662a85f47c4eefdb579e3871e5d
rjlucasm <rjlucasmonteiro@gmail.com>
2023-04-14 13:58:12 -0300

    Image fix

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   reactStrictMode: true,
-  images: {
-    loader: 'imgix',
-    path: "",
-  },
+  // images: {
+  //   loader: 'imgix',
+  //   path: "",
+  // },
   
   trailingSlash: true,
   // exportPathMap: async function () {
