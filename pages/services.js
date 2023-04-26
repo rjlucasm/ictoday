8306c92176cd2c9baeb98273b1b77241e14620b2
rjlucasm <rjlucasmonteiro@gmail.com>
2023-04-26 15:07:30 -0300

    IDgeek

--- pages/services.js
+++ pages/services.js
@@ -90,6 +90,9 @@ export default function Services() {
                 <a href="https://icp.guide/" target="_blank" rel="noopener noreferrer">ICP Guide</a> - The definitive guide to the Internet Computer Protocol (ICP)
             </div>
             <div className="pagescard">
+                <a href="https://icp.guide/" target="_blank" rel="noopener noreferrer">IDgeek</a> - A secure, automated and decentralized marketplace for buying or selling Internet Identity Anchors
+            </div>
+            <div className="pagescard">
                 <a href="https://identity.ic0.app/" target="_blank" rel="noopener noreferrer">Internet Identity</a> - Sign in to dapps on the IC and sites across the web anonymously and securely
             </div>
             <div className="pagescard">
